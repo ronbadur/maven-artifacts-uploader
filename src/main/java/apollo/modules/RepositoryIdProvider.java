3c1dfa040bb4cbdf8ae221651696b7302cde9bf7
Ron Badur <ronbadur02@gmail.com>
2018-01-15 00:03:24 +0200

    change group id and fix configuration problem

--- src/main/java/apollo/modules/RepositoryIdProvider.java
+++ src/main/java/apollo/modules/RepositoryIdProvider.java
@@ -2,9 +2,13 @@ package apollo.modules;
 
 import com.google.inject.Provider;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class RepositoryIdProvider implements Provider<String> {
@@ -17,8 +21,10 @@ public class RepositoryIdProvider implements Provider<String> {
         Properties properties = new Properties();
 
         try {
-            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(CONFIGURATION_FILENAME);
-            properties.load(inputStream);
+            Path currentPath = Paths.get(".").toAbsolutePath().normalize();
+            Path configurationFilePath = currentPath.resolve("conf").resolve(CONFIGURATION_FILENAME);
+            BufferedReader bufferedReader = Files.newBufferedReader(configurationFilePath);
+            properties.load(bufferedReader);
         } catch (IOException e) {
             throw new UncheckedIOException("there is problem with " + CONFIGURATION_FILENAME, e);
         }
--- src/main/java/apollo/modules/RepositoryUrlProvider.java
+++ src/main/java/apollo/modules/RepositoryUrlProvider.java
@@ -3,6 +3,9 @@ package apollo.modules;
 import com.google.inject.Provider;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class RepositoryUrlProvider implements Provider<String> {
@@ -15,10 +18,12 @@ public class RepositoryUrlProvider implements Provider<String> {
         Properties properties = new Properties();
 
         try {
-            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(CONFIGURATION_FILENAME);
-            properties.load(inputStream);
+            Path currentPath = Paths.get(".").toAbsolutePath().normalize();
+            Path configurationFilePath = currentPath.resolve("conf").resolve(CONFIGURATION_FILENAME);
+            BufferedReader bufferedReader = Files.newBufferedReader(configurationFilePath);
+            properties.load(bufferedReader);
         } catch (IOException e) {
-            throw new UncheckedIOException("there is problem with " + CONFIGURATION_FILENAME, e);
+            throw new UncheckedIOException("there is problem with " + CONFIGURATION_FILENAME + " file", e);
         }
 
         return DEPLOY_OPTION + properties.getProperty("repository.url");
